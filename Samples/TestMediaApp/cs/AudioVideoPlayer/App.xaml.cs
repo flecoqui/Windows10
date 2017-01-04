d2ff770b866fa3a384f67ffdcad094165eac786b
Frédéric Le Coquil <flecoqui@microsoft.com>
2017-01-04 10:08:00 +0100

    Add TestMediaApp Application published on Windows Store

---
+++ Samples/TestMediaApp/cs/AudioVideoPlayer/App.xaml.cs
@@ -0,0 +1,339 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.System;
+
+namespace AudioVideoPlayer
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Construct();
+            this.Suspending += OnSuspending;
+            // Initialize Log Message List
+            MessageList = new System.Collections.Concurrent.ConcurrentQueue<string>();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+
+
+
+        #region RS1
+        /// <summary>
+        /// Flag to indicate whether the app is currently in
+        /// foreground or background mode.
+        /// </summary>
+        /// <remarks>
+        /// This is used later to determine when to load / unload UI.
+        /// </remarks>
+        bool isInBackgroundMode;
+
+        /// <summary>
+        /// Called from App.xaml.cs when the application is constructed.
+        /// </summary>
+        void Construct()
+        {
+            // When an application is backgrounded it is expected to reach
+            // a lower memory target to maintain priority to keep running.
+            // Subscribe to the event that informs the app of this change.
+            MemoryManager.AppMemoryUsageIncreased += MemoryManager_AppMemoryUsageIncreased;
+
+            // Subscribe to key lifecyle events to know when the app
+            // transitions to and from foreground and background.
+            // Leaving the background is an important transition
+            // because we may need to restore UI.
+            EnteredBackground += App_EnteredBackground;
+            LeavingBackground += App_LeavingBackground;
+
+            // Subscribe to regular lifecycle events to display a toast notification
+            Suspending += App_Suspending;
+            Resuming += App_Resuming;
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void App_Suspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            // Optional: Save application state and stop any background activity
+            LogMessage("Suspending");
+            deferral.Complete();
+        }
+
+        /// <summary>
+        /// Invoked when application execution is resumed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void App_Resuming(object sender, object e)
+        {
+            LogMessage("Resuming");
+        }
+
+        /// <summary>
+        /// Handle system notifications that the app has increased its
+        /// memory usage level compared to its current target.
+        /// </summary>
+        /// <remarks>
+        /// The app may have increased its usage or the app may have moved
+        /// to the background and the system lowered the target the app
+        /// is expected to reach. Either way, if the application wants
+        /// to maintain its priority to avoid being suspended before
+        /// other apps, it needs to reduce its memory usage.
+        /// </remarks>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MemoryManager_AppMemoryUsageIncreased(object sender, object e)
+        {
+            // Optionally log a debug message describing the increase
+            LogMessage("Memory usage increased");
+
+            // Unload view content if needed. 
+            //
+            // Memory usage may have been fine when initially entering the background but
+            // a short time later memory targets could change or the app might
+            // just be using more memory and needs to trim back.
+            UnloadViewContentIfNeeded();
+        }
+
+        /// <summary>
+        /// The application entered the background.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void App_EnteredBackground(object sender, EnteredBackgroundEventArgs e)
+        {
+            // Place the application into "background mode" and note the
+            // transition with a flag.
+            LogMessage("Entered background");
+            isInBackgroundMode = true;
+
+            // Unload view content if needed.
+            //
+            // If the AppMemoryUsageLevel changed and triggered AppMemoryUsageIncreased
+            // before the app entered background mode then the view is still loaded
+            // and we need to unload it now.
+            UnloadViewContentIfNeeded();
+        }
+
+        /// <summary>
+        /// The application is leaving the background.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void App_LeavingBackground(object sender, LeavingBackgroundEventArgs e)
+        {
+            // Mark the transition out of background mode.
+            LogMessage("Leaving background");
+            isInBackgroundMode = false;
+
+            // Reastore view content if it was previously unloaded.
+            if (Window.Current.Content == null)
+            {
+                LogMessage("Loading view");
+                //    CreateRootFrame(ApplicationExecutionState.Running, string.Empty);
+            }
+        }
+
+        /// <summary>
+        /// Unload view content if needed.
+        /// </summary>
+        /// <remarks>
+        /// When the app enters the background or receives a memory usage increased
+        /// event it can optionally unload its view content in order to reduce
+        /// memory usage and the chance of being suspended.
+        /// 
+        /// This must be called from both event handlers because an application may already
+        /// be in a high memory usage state when entering the background or it
+        /// may be in a low memory usage state with no need to unload resources yet
+        /// and only enter a higher state later.
+        /// </remarks>
+        public void UnloadViewContentIfNeeded()
+        {
+            // Obtain the current usage level
+            var level = MemoryManager.AppMemoryUsageLevel;
+
+            // Check the usage level to determine what steps to take to reduce
+            // memory usage. The approach taken here is if the application is already
+            // over the limit, or if it's at a high usage level, go ahead and unload
+            // application views to recover memory. This is conceptually similar to
+            // what earlier dual process models did except here the process continues to
+            // run just without view content.
+            if (level == AppMemoryUsageLevel.OverLimit || level == AppMemoryUsageLevel.High)
+            {
+                // Unload view for extra memory if the application is currently
+                // in background mode and has a view with content.
+                if (isInBackgroundMode && Window.Current.Content != null)
+                {
+                    LogMessage("Unloading view");
+
+                    // Clear the view content and trigger the GC to recover resources.
+                    Window.Current.Content = null;
+                    GC.Collect();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a string describing current memory usage
+        /// </summary>
+        /// <returns>String describing current memory usage</returns>
+        private string GetMemoryUsageText()
+        {
+            return string.Format("[Memory: Level={0}, Usage={1}K, Target={2}K]",
+                MemoryManager.AppMemoryUsageLevel, MemoryManager.AppMemoryUsage / 1024, MemoryManager.AppMemoryUsageLimit / 1024);
+        }
+
+        /// <summary>
+        /// Sends a toast notification
+        /// </summary>
+        /// <param name="msg">Message to send</param>
+        /// <param name="subMsg">Sub message</param>
+        public void ShowToast(string msg, string subMsg = null)
+        {
+            if (subMsg == null)
+                subMsg = GetMemoryUsageText();
+
+            System.Diagnostics.Debug.WriteLine(msg + "\n" + subMsg);
+
+
+            var toastXml = Windows.UI.Notifications.ToastNotificationManager.GetTemplateContent(Windows.UI.Notifications.ToastTemplateType.ToastText02);
+
+            var toastTextElements = toastXml.GetElementsByTagName("text");
+            toastTextElements[0].AppendChild(toastXml.CreateTextNode(msg));
+            toastTextElements[1].AppendChild(toastXml.CreateTextNode(subMsg));
+
+            var toast = new Windows.UI.Notifications.ToastNotification(toastXml);
+            Windows.UI.Notifications.ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+
+
+        #endregion
+
+
+
+        #region Logs
+        public System.Collections.Concurrent.ConcurrentQueue<String> MessageList;
+        /// <summary>
+        /// Display Message on the application page
+        /// </summary>
+        /// <param name="Message">String to display</param>
+        void LogMessage(string Message)
+        {
+            if (MessageList == null)
+                MessageList = new System.Collections.Concurrent.ConcurrentQueue<string>();
+            string Text = string.Format("{0:d/M/yyyy HH:mm:ss.fff}", DateTime.Now) + " " + Message + "\n";
+            MessageList.Enqueue(Text);
+            System.Diagnostics.Debug.WriteLine(Text);
+        }
+        #endregion
+
+
+    }
+}
---
+++ Samples/TestMediaApp/cs/MediaHelpers/PlayReadyHelper.cpp
@@ -0,0 +1,117 @@
+﻿#include "pch.h"
+#include "PlayReadyHelper.h"
+#include <wrl.h>
+#include <wrl/client.h>
+#include <wrl\implements.h>
+#include <dxgi.h>
+#include <dxgi1_2.h>
+#include <dxgi1_3.h>
+#include <d3d11_1.h>
+#include <d2d1_2.h>
+#include "D3d11.h"
+
+using namespace MediaHelpers;
+using namespace Microsoft::WRL;
+using namespace Windows::Media::Devices;
+using namespace Platform;
+using namespace Windows::Media::Protection::PlayReady;
+
+PlayReadyHelper::PlayReadyHelper()
+{
+}
+
+/// <summary>
+/// Retrieve the PlayReady license expiration date 
+/// The use of this method is a turn around to a PlayReady issue with .Net Native.
+/// </summary>
+Platform::IBox<Windows::Foundation::DateTime>^ PlayReadyHelper::GetLicenseExpirationDate(IPlayReadyLicense^ license)
+{
+	//
+	// The following code is a workaround for a bug in PlayReady.
+	//
+	Platform::Object^ boxedExpirationDate = license->ExpirationDate;
+	if (boxedExpirationDate != nullptr)
+	{
+		return ((IBox<Windows::Foundation::DateTime>^)boxedExpirationDate)->Value;
+	}
+	return nullptr;
+}
+/// <summary>
+/// Return true if the platform does support Hardware DRM 
+/// </summary>
+bool PlayReadyHelper::IsHardwareDRMSupported(void)
+{
+	HRESULT hr;
+	ComPtr<IDXGIFactory2> dxgiFactory;
+	ComPtr<IDXGIAdapter> dxgiAdapter;
+	ComPtr<IDXGIDevice1> dxgiDevice;		
+	ComPtr<ID3D11Device>        d3dDevice;
+	ComPtr<ID3D11DeviceContext> d3dContext;
+	ComPtr<ID3D11VideoDevice> d3dVideoDevice;
+
+	UINT factoryFlags = 0;
+	hr = CreateDXGIFactory2(factoryFlags, __uuidof(IDXGIFactory2), &dxgiFactory);
+	if (hr == S_OK)
+	{
+		UINT creationFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT | D3D11_CREATE_DEVICE_VIDEO_SUPPORT;
+
+		d3dDevice = nullptr;
+		UINT i_adapter = 0;
+		while (d3dDevice == nullptr)
+		{
+			hr = dxgiFactory->EnumAdapters(i_adapter++, &dxgiAdapter);
+			if (hr == S_OK)
+			{
+				hr = D3D11CreateDevice(
+					dxgiAdapter.Get(),
+					D3D_DRIVER_TYPE_UNKNOWN,
+					nullptr,
+					creationFlags,
+					NULL,
+					0,
+					D3D11_SDK_VERSION,
+					&d3dDevice,
+					nullptr,
+					&d3dContext
+					);
+				if (FAILED(hr))
+					d3dDevice = nullptr;
+			}
+			else
+				break;
+		}
+		if (hr != S_OK)
+			return false;
+
+		hr = d3dDevice.As(&d3dVideoDevice);
+		if (hr != S_OK)
+			return false;
+
+		UINT count = d3dVideoDevice->GetVideoDecoderProfileCount();
+		for (UINT i = 0; i < count; i++)
+		{
+			GUID DecoderProfile;
+			D3D11_VIDEO_CONTENT_PROTECTION_CAPS protectionCaps;
+			hr = d3dVideoDevice->GetVideoDecoderProfile(
+				i,
+				&DecoderProfile
+				);
+			if (hr == S_OK)
+			{
+				hr = d3dVideoDevice->GetContentProtectionCaps(
+					NULL,
+					&DecoderProfile,
+					&protectionCaps
+					);
+				if (hr == S_OK)
+				{
+					//	D3D11_CONTENT_PROTECTION_CAPS_SOFTWARE = 0x1,
+					//	D3D11_CONTENT_PROTECTION_CAPS_HARDWARE = 0x2,
+					if (protectionCaps.Caps & D3D11_CONTENT_PROTECTION_CAPS_HARDWARE)
+						return true;
+				}
+			}
+		}
+	}
+	return false;
+}
